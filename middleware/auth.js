48afde7eb2bea0cbd9ab1bb860a9f78ec2c91536
Awarden1 <thelab06x@gmail.com>
2023-08-09 23:25:04 +0100

    adding Auth middle ware feature

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async function(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  try {
+    await jwt.verify(token, process.env.jwtSecret, (error, decoded)=>{
+      if(error){
+        res.status(401).json({ message: 'Token is not valid' });
+      }
+      else{
+        req.id = decoded.user.id;
+        next();
+      }
+    });
+  } catch (err) {
+    res.clearCookie("token");
+    res.status(505).json({ message: 'Server Error' });
+  }
+};
