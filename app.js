bbd9043d9122bae5dbbc985ae628dbc34d496ee2
Awarden1 <thelab06x@gmail.com>
2023-08-09 23:06:08 +0100

    server setup and mongodb connected

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const app = express();
+const connectDB = require('./db');
+
+require('dotenv').config()
+
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Api is running');
+});
+
+// app.use('/api/auth', require('./exampsub folder/api/auth'));
+// app.use('/api/task', require('./exampsub folder/api/task'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Running on the port ${port}`);
+});
