28df8f3c0accb925ff1edf6adc5d1d29973d4242
Awarden1 <thelab06x@gmail.com>
2023-08-10 22:30:58 +0100

    splitting folder

--- server/test/security-test.js
+++ server/test/security-test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../../app')
+const app = require('../app')
 
 describe('Auth Api', () => {
     it('should create a new user', async () => {
